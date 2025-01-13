2630fc7fb16ef0296562dbac3fd20496f8fce1eb
lichun <2747517125@qq.com>
2025-01-13 09:59:03 +0800

    feat:单机并发缓存

---
+++ cache.go
@@ -0,0 +1,36 @@
+// cache 对LRU 缓存的封装, 并加入并发控制
+package leecache
+
+import (
+	"leecache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return 
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
+
